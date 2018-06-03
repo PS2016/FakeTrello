b0e21be4587fc573d4da53563f9265f20030caf6
Dorohin Bogdan <bogdan94@live.ru>
2018-06-03 11:19:30 +0300

    Refactoring + Autofac config

---
+++ FT.Data/FT.Api/Config/AutofacConfig.cs
@@ -0,0 +1,26 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using Autofac.Integration.WebApi;
+using FT.Data;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace FT.Api.Config
+{
+    public static class AutofacConfig
+    {
+        public static void Configure(HttpConfiguration config)
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterApiControllers(typeof(WebApiApplication).Assembly);
+
+            builder.RegisterType<FTContext>().InstancePerLifetimeScope().AsSelf();
+
+            //Register service HERE
+
+            var container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+        }
+    }
+}
