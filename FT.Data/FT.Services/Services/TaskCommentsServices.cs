de3381a9918970f6f4a5fd61f58a2f9c8b6499b8
student <student@209-02>
2018-06-03 11:24:25 +0300

    Progress Service TaskCommentApiModel

--- FT.Data/FT.Services/Services/TaskCommentsServices.cs
+++ FT.Data/FT.Services/Services/TaskCommentsServices.cs
@@ -1,4 +1,5 @@
-﻿using FT.Data;
+﻿using FT.Api.Model;
+using FT.Data;
 using FT.Services.Base;
 using System;
 using System.Collections.Generic;
@@ -12,6 +13,20 @@ namespace FT.Services.Services
     {
          public TaskCommentsServices(FTContext context):base(context)
         {
+        }
+        public TaskCommentApiModel Get(Guid id)
+        {
+            var comment = _context.TaskComment.FirstOrDefault(c => c.TaskId == id);
+            if (comment == null) return null;
+            return AutoMapperConfig.Mapper.Map<TaskComment, TaskCommentApiModel>(comment);
+        }
+        public TaskCommentApiModel Create(TaskCommentApiModel model)
+        {
+            var message = AutoMapperConfig.Mapper.Map<TaskCommentApiModel,TaskComment>(model);
+            _context.TaskComment.Add(message);
+            _context.SaveChanges();
+
+            return AutoMapperConfig.Mapper.Map<TaskCommentApiModel, TaskComment>(message);
         }
     }
 }
