82431ee933e1e7334901e70e9e1beff5c8b0deaf
student <student@209-02>
2018-06-03 10:42:28 +0300

    Init model User

--- FT.Data/FT.Data/Model/User.cs
+++ FT.Data/FT.Data/Model/User.cs
@@ -11,7 +11,20 @@ namespace FT.Data.DataModel
 {
     public class User
     {
-       
+       [Key]
+       public Guid Id { get; set; }
+        [Required][MaxLength(33)]
+        public string FirstName { get; set; }
+        [Required] [MaxLength(33)]
+        public string LastName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required][MaxLength(256)][MinLength(8)]
+        public string HashPassword { get; set;}
+        public DateTime? LastLogin { get; set; }
+        #region
+        public virtual ICollection<Task> Tasks { get; set; } = new HashSet<Task>();
+        #endregion
     }
 
 }
