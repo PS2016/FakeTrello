4c324332393d76648a9cf609d2983bb85032623b
student <student@209-03>
2018-06-03 10:34:56 +0300

    Api Models removed unnesesasry things

--- FT.Data/FT.Api.Model/Base/ListResponse.cs
+++ FT.Data/FT.Api.Model/Base/ListResponse.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
-namespace FT.Api.Model 
+namespace FT.Api.Model
 {
     public class ListResponse<TModel>:ResponseBase
     {
