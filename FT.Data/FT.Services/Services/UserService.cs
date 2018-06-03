801be97b9649eba423bdca831080f4b690218984
student <student@209-07>
2018-06-03 10:53:21 +0300

    init serv

--- FT.Data/FT.Services/Services/UserService.cs
+++ FT.Data/FT.Services/Services/UserService.cs
@@ -1,31 +1,29 @@
 ﻿using FT.Api.Model;
-using FT.Data.DataModel;
+using FT.Data;
+using FT.Services.Base;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace FT.Services.Services
-{
-    public class UserService
-    {
-        public UserService()
-        {
+//namespace FT.Services.Services
+//{
+//    public class UserService:ServiceBase<FTContext>
+//    {
+//        public UserService():base(FTContext context)
+//        {
 
-        }
-        public UserApiModel Add(UserApiModel NewUser)
-        {
-            User user = new User { };
-            return NewUser;
-        }
-        public UserApiModel Update(UserApiModel NewUser)
-        {
-            return NewUser;
-        }
-        public UserApiModel get(Guid UserId)
-        {
-            return null;
-        }
-    }
-}
+//        }
+//        public UserApiModel Add(UserApiModel NewUser)
+//        {
+//            User user = new User {Id=NewUser.Id,Email=NewUser.Email,FirstName=NewUser.FirstName,LastName=NewUser.LastName,HashPassword=NewUser.HashPassword,LastLogin=NewUser.LastLogin };
+            
+//            return NewUser;
+//        }
+//        public UserApiModel Update(UserApiModel NewUser)
+//        {
+//            return NewUser;
+//        }
+//        public UserApiModel get(Guid UserId)
+//        {
+//            return null;
+//        }
+//    }
+//}
