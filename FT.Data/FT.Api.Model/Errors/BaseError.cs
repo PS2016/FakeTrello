65ec8ceb39ebc4c869f85d99b816ad282d64dc82
student <student@209-03>
2018-06-03 10:27:40 +0300

    Response Models Added

---
+++ FT.Data/FT.Api.Model/Errors/BaseError.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FT.Api.Model
+{
+    public class BaseError
+    {
+        public string  Message { get; set; }
+        public string  StackTrace { get; set; }
+    }
+}
